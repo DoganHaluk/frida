2cd5224d269407e78b3315024163b33e77b23498
DoganHaluk <ha_look@yahoo.com>
2021-04-21 16:05:30 +0200

    Frida frituur webpage

--- src/main/java/be/vdab/frida/controllers/IndexController.java
+++ src/main/java/be/vdab/frida/controllers/IndexController.java
@@ -1,30 +1,19 @@
 package be.vdab.frida.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 
-@RestController
+@Controller
 @RequestMapping("/")
 class IndexController {
     @GetMapping
-    public String index() {
-        var openOfGesloten = LocalDate.now().getDayOfWeek() == DayOfWeek.MONDAY ? "Gesloten" : "Open";
-        return "<!DOCTYPE html>" +
-                "<html lang='nl'>" +
-                "  <head>" +
-                "    <meta charset='UTF-8'>" +
-                "    <title>Frituur Frida</title>" +
-                "    <link rel='icon' href='images/frida.ico' type='image/x-icon'>" +
-                "    <link rel='stylesheet' href='css/frida.css'>" +
-                "  </head>" +
-                "  <body>" +
-                "    <h1>Frituur Frida</h1>"
-                    + openOfGesloten +
-                "  </body>" +
-                "</html>";
+    public ModelAndView index() {
+        var openOfGesloten = LocalDate.now().getDayOfWeek() == DayOfWeek.MONDAY ? "gesloten" : "open";
+        return new ModelAndView("index", "dag", openOfGesloten);
     }
 }
