f3933919afff0aa746293177028481ab490388e5
DoganHaluk <ha_look@yahoo.com>
2021-04-30 22:08:15 +0200

    Frida frituur webpage

--- src/main/java/be/vdab/frida/controllers/TaalController.java
+++ src/main/java/be/vdab/frida/controllers/TaalController.java
@@ -5,9 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.thymeleaf.standard.processor.StandardAltTitleTagProcessor;
-
-import java.util.Arrays;
 
 @Controller
 @RequestMapping("talen")
