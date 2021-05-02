9f3a3f781f347cca134b3b6c4073add8f70c72c4
DoganHaluk <ha_look@yahoo.com>
2021-05-02 23:57:47 +0200

    Frida frituur webpage

--- src/main/java/be/vdab/frida/controllers/IndexController.java
+++ src/main/java/be/vdab/frida/controllers/IndexController.java
@@ -3,12 +3,16 @@ package be.vdab.frida.controllers;
 import be.vdab.frida.domain.Adres;
 import be.vdab.frida.domain.Gemeente;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -18,6 +22,18 @@ class IndexController {
         var openOfGesloten = LocalDate.now().getDayOfWeek() == DayOfWeek.MONDAY ? "gesloten" : "open";
         var modelAndView = new ModelAndView("index", "dag", openOfGesloten);
         modelAndView.addObject("zaakAdres", new Adres("Waaslandstraat", "51", new Gemeente("Hoboken", 2660)));
+        return modelAndView;
+    }
+
+    @GetMapping
+    public ModelAndView index(@CookieValue Optional<Integer> aantalBezoeken, HttpServletResponse response) {
+        var modelAndView = new ModelAndView("index");
+        var nieuwAantalBezoeken = aantalBezoeken.orElse(0) + 1;
+        var cookie = new Cookie("aantalBezoeken", String.valueOf(nieuwAantalBezoeken));
+        cookie.setMaxAge(31_536_000);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        modelAndView.addObject("aantalBezoeken", nieuwAantalBezoeken);
         return modelAndView;
     }
 }
