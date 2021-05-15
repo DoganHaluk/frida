51a249470bb39f6054a78beb994b87d3d13b1758
DoganHaluk <ha_look@yahoo.com>
2021-05-15 23:34:56 +0200

    Frida frituur webpage

---
+++ src/main/java/be/vdab/frida/repositories/CSVSausRepository.java
@@ -0,0 +1,9 @@
+package be.vdab.frida.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CSVSausRepository implements SausRepository{
+    private final
+
+}
