ba1443d16279e1920a40ad27774b972c4d55d8ac
DoganHaluk <ha_look@yahoo.com>
2021-05-17 19:25:35 +0200

    Frida frituur webpage

--- src/main/java/be/vdab/frida/controllers/SausController.java
+++ src/main/java/be/vdab/frida/controllers/SausController.java
@@ -1,6 +1,7 @@
 package be.vdab.frida.controllers;
 
 import be.vdab.frida.domain.Saus;
+import be.vdab.frida.services.SausService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,47 +15,33 @@ import java.util.stream.Collectors;
 @Controller
 @RequestMapping("sauzen")
 class SausController {
-    private final Saus[] sauzen = {
-            new Saus(1, "cocktail", new String[]{"mayonaise", "ketchup", "cognac"}),
-            new Saus(2, "mayonaise", new String[]{"ei", "mosterd"}),
-            new Saus(3, "mosterd", new String[]{"mosterd", "azijn", "witte wijn"}),
-            new Saus(4, "tartare", new String[]{"mayonaise", "augurk", "tabasco"}),
-            new Saus(5, "vinaigrette", new String[]{"olijfolie", "mosterd", "azijn"})};
+    private final char[] alfabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    private final SausService sausService;
+
+    public SausController(SausService sausService) {
+        this.sausService = sausService;
+    }
 
     @GetMapping
     public ModelAndView sauzen() {
-        return new ModelAndView("sauzen", "sauzen", sauzen);
+        return new ModelAndView("sauzen", "sauzen", sausService.findAll());
     }
 
     @GetMapping("{id}")
     public ModelAndView saus(@PathVariable long id) {
         var modelAndView = new ModelAndView("saus");
-        Arrays.stream(sauzen).filter(saus -> saus.getId() == id).findFirst()
-                .ifPresent(saus -> modelAndView.addObject("saus", saus));
+        sausService.findById(id).ifPresent(saus -> modelAndView.addObject(saus));
         return modelAndView;
-    }
-
-    private List<Character> alfabetSaus(){
-        return Arrays.stream(sauzen)
-                .map(saus -> saus.getNaam().charAt(0))
-                .distinct()
-                .sorted()
-                .collect(Collectors.toList());
     }
 
     @GetMapping("alfabet")
     public ModelAndView alfabet() {
-        return new ModelAndView("sausAlfabet", "alfabet", alfabetSaus());
-    }
-
-    private List<Saus> sauzenDieBeginnenMet(char letter) {
-        return Arrays.stream(sauzen)
-                .filter(saus -> saus.getNaam().charAt(0) == letter)
-                .collect(Collectors.toList());
+        return new ModelAndView("sausAlfabet", "alfabet", alfabet);
     }
 
     @GetMapping("alfabet/{letter}")
     public ModelAndView sauzenBeginnendMet(@PathVariable char letter) {
-        return new ModelAndView("sausAlfabet", "alfabet", alfabetSaus()).addObject("sauzen", sauzenDieBeginnenMet(letter));
+        return new ModelAndView("sausAlfabet", "alfabet", alfabet)
+                .addObject("sauzen", sausService.findByNaamBegintMet(letter));
     }
 }
