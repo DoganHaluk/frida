8dca2d3c6224fc44ad3f2e0b1bb340327c8591f5
DoganHaluk <ha_look@yahoo.com>
2021-05-22 04:59:07 +0200

    Frida frituur webpage

--- src/main/java/be/vdab/frida/repositories/CSVSausRepository.java
+++ src/main/java/be/vdab/frida/repositories/CSVSausRepository.java
@@ -3,42 +3,46 @@ package be.vdab.frida.repositories;
 import be.vdab.frida.domain.Saus;
 import be.vdab.frida.exceptions.SausRepositoryException;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 @Qualifier("CSV")
-public class CSVSausRepository implements SausRepository {
-    private static final Path PAD = Paths.get("data/sauzen.csv");
+class CSVSausRepository implements SausRepository {
+    private final Path pad;
+
+    CSVSausRepository(@Value("${CSVSausenPad}") Path pad) {
+        this.pad = pad;
+    }
 
     @Override
     public List<Saus> findAll() {
         try {
-            return Files.lines(PAD)
+            return Files.lines(pad)
                     .map(this::maakSaus)
                     .collect(Collectors.toList());
         } catch (IOException ex) {
-            throw new SausRepositoryException("Faut bij lezen " + PAD);
+            throw new SausRepositoryException("Faut bij lezen " + pad);
         }
     }
 
     private Saus maakSaus(String regel) {
         var onderdelen = regel.split(",");
         if (onderdelen.length < 2) {
-            throw new SausRepositoryException(PAD + ":" + regel + ": minder dan 2 onderdelen");
+            throw new SausRepositoryException(pad + ":" + regel + ": minder dan 2 onderdelen");
         }
         try {
             var ingredienten = Arrays.copyOfRange(onderdelen, 2, onderdelen.length);
             return new Saus(Long.parseLong(onderdelen[0]), onderdelen[1], ingredienten);
         } catch (NumberFormatException ex) {
-            throw new SausRepositoryException(PAD + ":" + regel + ": verkeerde id");
+            throw new SausRepositoryException(pad + ":" + regel + ": verkeerde id");
         }
     }
 }
