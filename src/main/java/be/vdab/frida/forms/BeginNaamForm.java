90477d30d6439ee199fdd759677a0ecfd7ab5d79
DoganHaluk <ha_look@yahoo.com>
2021-06-06 01:43:19 +0200

    Frida frituur webpage

--- src/main/java/be/vdab/frida/forms/BeginNaamForm.java
+++ src/main/java/be/vdab/frida/forms/BeginNaamForm.java
@@ -2,7 +2,6 @@ package be.vdab.frida.forms;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 public class BeginNaamForm {
     @NotEmpty
