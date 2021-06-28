10a86694d652158a98664771e2b172a15bbe6b70
DoganHaluk <ha_look@yahoo.com>
2021-06-28 23:58:22 +0200

    Frida frituur webpage

---
+++ src/main/java/be/vdab/frida/controllers/FrietController.java
@@ -0,0 +1,5 @@
+package be.vdab.frida.controllers;
+
+class FrietController {
+
+}
---
+++ src/main/java/be/vdab/frida/sessions/ZoekDeFriet.java
@@ -0,0 +1,7 @@
+package be.vdab.frida.sessions;
+
+import java.io.Serializable;
+
+public class ZoekDeFriet implements Serializable {
+
+}
