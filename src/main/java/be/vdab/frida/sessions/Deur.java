052af7fca352a180a37b1b512618ac006e1800d1
DoganHaluk <ha_look@yahoo.com>
2021-06-29 23:51:46 +0200

    Frida frituur webpage

--- src/main/java/be/vdab/frida/sessions/Deur.java
+++ src/main/java/be/vdab/frida/sessions/Deur.java
@@ -1,7 +1,21 @@
 package be.vdab.frida.sessions;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
 import java.io.Serializable;
 
+@Component
+@SessionScope
 public class Deur implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private int deur;
 
+    public int getDeur() {
+        return deur;
+    }
+
+    public void setDeur(int deur) {
+        this.deur = deur;
+    }
 }
--- src/main/java/be/vdab/frida/sessions/ZoekDeFriet.java
+++ src/main/java/be/vdab/frida/sessions/ZoekDeFriet.java
@@ -1,7 +1,21 @@
 package be.vdab.frida.sessions;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
 import java.io.Serializable;
 
+@Component
+@SessionScope
 public class ZoekDeFriet implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private long geopendeDeur;
 
+    public long getGeopendeDeur() {
+        return geopendeDeur;
+    }
+
+    public void setGeopendeDeur(long geopendeDeur) {
+        this.geopendeDeur = geopendeDeur;
+    }
 }
