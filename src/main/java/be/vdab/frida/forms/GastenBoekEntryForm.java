624e8727f34c2e95a653bef74b7f1f2a9a3d559f
DoganHaluk <ha_look@yahoo.com>
2021-07-21 02:15:32 +0200

    Frida frituur webpage

--- src/main/java/be/vdab/frida/forms/GastenBoekEntryForm.java
+++ src/main/java/be/vdab/frida/forms/GastenBoekEntryForm.java
@@ -4,7 +4,7 @@ import be.vdab.frida.domain.GastenBoekEntry;
 
 import java.time.LocalDate;
 
-public class GastenboekEntryForm extends GastenBoekEntry {
+public class GastenBoekEntryForm extends GastenBoekEntry {
     public GastenBoekEntryForm(String naam, String bericht) {
         super(0, naam, LocalDate.now(), bericht); }
 }
